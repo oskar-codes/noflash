ce915f9dd55bc3839e24739f77545df46a64a66d
Oskar Codes <oskarbertrand@icloud.com>
2020-05-23 17:12:50 +0200

    Update index.js

--- .github/actions/notify/index.js
+++ .github/actions/notify/index.js
@@ -1,1 +1,12 @@
+var request = new XMLHttpRequest();
+request.open("POST", "https://discord.com/api/webhooks/713771249204985966/9J8NW-_UijRM09swDrjeMmssHN9F5CMvfNG4IIxUvkjTmJN9Im-OpkyLMIQE979H90UJ");
 
+request.setRequestHeader('Content-type', 'application/json');
+
+var params = {
+  username: "Github Pages",
+  avatar_url: "",
+  content: "Github Pages has finished building."
+}
+
+request.send(JSON.stringify(params));
